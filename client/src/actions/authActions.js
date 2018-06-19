42e8b1558313f21c00f7ef35d02e0ae5744ee0b8
ViacheslavPr <bogdanov_slava_90@mail.ru>
2018-06-19 13:02:02 +0300

    Logout and navbar links

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -47,3 +47,13 @@ export const setCurrentUser = decoded => {
     payload: decoded
   };
 };
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  //Remove token from loacalStorage
+  localStorage.removeItem("jwtToken");
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} with will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+};
