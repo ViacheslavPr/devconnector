461090557c68e85ffedee42419580e5d772156a7
ViacheslavPr <bogdanov_slava_90@mail.ru>
2018-06-26 14:55:57 +0300

    Profile by handle and sub components

---
+++ client/src/components/profile/ProfileHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProfileHrader extends Component {
+  render() {
+    return (
+      <div>
+        <h1>PROFILE HEADER</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileHrader;
